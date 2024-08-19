26e9dea68950d759123c3c0f1fbf6e853b3ead9f
Ruslan Semak <ruslan.semack1501@gmail.com>
2024-08-19 19:07:07 +0300

    Добавлены dto

---
+++ src/dto/index.ts
@@ -0,0 +1,2 @@
+export * from './task.dto';
+export * from './id.number.dto';
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,54 @@
+import 'reflect-metadata';
+
+import { plainToInstance } from 'class-transformer';
+import { validateSync } from 'class-validator';
 import express from 'express';
 
+import { CreateTaskDto, IdNumberDto } from './dto';
 import { privateGuard } from './guard/private.guard';
 import { logMiddleware } from './middlewares';
 import { errorHandler } from './middlewares/error.handler';
 
 const server = express();
 
+server.use(express.json());
 server.use(logMiddleware);
 server.use(privateGuard);
 
-server.get('/task', (req, res) => {
-  console.log('Начало обработки запроса!');
-  if (Math.random() > 0.5) {
-    throw Error('Шанс на ошибку сработал!');
+server.post('/task', (req, res) => {
+  const body = plainToInstance(CreateTaskDto, req.body);
+  const errors = validateSync(body);
+
+  if (errors.length) {
+    const constraints = errors[0].constraints;
+    let message = 'Unknown validation error';
+
+    if (constraints) {
+      message = constraints[Object.keys(constraints)[0]];
+    }
+
+    throw Error(message);
   }
 
-  res.send('Hello World!');
+  res.json({ id: 1, ...body });
+});
+
+server.get('/task/:id', (req, res) => {
+  const params = plainToInstance(IdNumberDto, req.params);
+  const errors = validateSync(params);
+
+  if (errors.length) {
+    const constraints = errors[0].constraints;
+    let message = 'Unknown validation error';
+
+    if (constraints) {
+      message = constraints[Object.keys(constraints)[0]];
+    }
+
+    throw Error(message);
+  }
+
+  res.json({ id: params.id });
 });
 
 server.use(errorHandler);
