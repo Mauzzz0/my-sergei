7a2b94dd402b4ca205909f51c8c54bf511cbbb29
Ruslan Semak <ruslan.semack1501@gmail.com>
2024-08-19 17:10:39 +0300

    Добавлен express

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,27 @@
-console.log('Эта программа для просмотра видео, и Изменения из мастера!!!');
-console.log('что-то 1, 3');
+import express from 'express';
+
+import { logRoutes } from './log.routes';
+
+const server = express();
+
+server.use(express.json());
+
+const port = 2000;
+
+const userRouter = express.Router();
+
+userRouter.get('/profile', (req, res) => {
+  res.json({ id: 10, name: 'chris' });
+});
+
+userRouter.get('/:name', (req, res) => {
+  res.json({ name: req.params.name });
+});
+
+server.use('/user', userRouter);
+
+logRoutes(server);
+
+server.listen(port, () => {
+  console.log(`Сервер запущен на порту ${port}`);
+});
