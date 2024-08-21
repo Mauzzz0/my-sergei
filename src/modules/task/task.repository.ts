64a8749194dcc0b94c1864ca464ffc165c55a02c
Ruslan Semak <ruslan.semack1501@gmail.com>
2024-08-21 18:53:46 +0300

    Добавлена база данных в файле

--- src/modules/task/task.repository.ts
+++ src/modules/task/task.repository.ts
@@ -1,6 +1,17 @@
+import { existsSync, readFileSync, writeFileSync } from 'node:fs';
+
 import { Task } from './task.types';
 
-const storage: Task[] = [];
+let storage: Task[] = [];
+
+const filename = 'tasks.json';
+const writeStorageToFile = () => writeFileSync(filename, JSON.stringify(storage));
+
+if (existsSync(filename)) {
+  storage = JSON.parse(readFileSync(filename, 'utf-8'));
+} else {
+  writeStorageToFile();
+}
 
 export const TaskRepository = {
   getById(id: Task['id']): Task | undefined {
@@ -11,6 +22,7 @@ export const TaskRepository = {
     const maxId = storage.sort((a, b) => (a.id < b.id ? 1 : -1))[0]?.id ?? 0;
 
     storage.push({ ...task, id: maxId + 1 });
+    writeStorageToFile();
 
     return storage[storage.length - 1];
   },
